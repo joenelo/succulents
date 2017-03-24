503856952268b0099fe6bf227a9b2c54d4cbfe12
joenelo <joeandrewnelson@gmail.com>
2017-03-23 22:22:56 -0700

    Responsiveness completed. Animations work in all sizes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,8 @@ $(document).ready(function(){
     var logo = $('.logo'),
         btn1 = $('.btn-full'),
         btn2 = $('.btn-ghost'),
-          h1 = $('h1');
+          h1 = $('h1'),
+    mainNav = $('.main-nav');
 
 
     TweenLite.from('.logo', 1.25, {autoAlpha:0, height:10});
@@ -15,19 +16,31 @@ $(document).ready(function(){
     TweenLite.from('.btn-ghost', 2, {y: -800, delay: 2.5});
     TweenLite.from('.main-nav', 1.25, {x: 600, delay:4.5});
 
-    //Init ScrollMagic
     var controller = new ScrollMagic.Controller();
+    // build tween
+    var tween = TweenMax.to("#twoDImage", 0.5, {scale: 2, repeat: 5, yoyo: true});
+
+    // build scene and set duration to window height
+    var scene = new ScrollMagic.Scene({triggerElement: "#top-section", duration: "100%"})
+        .setTween(tween)
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
 
     //-----------------------------------------------//
     //--Lower Mid Section Boxes Slide in from side --//
     //-----------------------------------------------//
 
-    var ourScene = new ScrollMagic.Scene({
+
+    // ---- build tween for first Show Me More box ----- //
+    var swipe1Tween = TweenMax.from("#show-me-more", 1.5,
+        {x: -1200, autoAlpha: 0}
+    );
+
+    var swipe1Scene = new ScrollMagic.Scene({
         triggerElement: '.long-copy',
         triggerHook: 0.3
-
     })
-        .setClassToggle('#show-me-more', 'fade-in')
+        .setTween(swipe1Tween)
         .addIndicators({
             name: 'info box movements',
             colorTrigger: 'black',
@@ -35,31 +48,44 @@ $(document).ready(function(){
         })
         .addTo(controller);
 
-    var ourScene2 = new ScrollMagic.Scene({
-        triggerElement: '.first-box .long-copy1',
-        triggerHook: 0.4
 
+
+    // ----  build tween for  Show Me More 2 box ----- //
+    var swipe2Tween = TweenMax.from("#show-me-more-2", 1.5,
+        {x: +1200, autoAlpha: 0}
+    );
+
+    var swipe2Scene = new ScrollMagic.Scene({
+        triggerElement: '.first-box',
+        triggerHook: 0.4
     })
-        .setClassToggle('#show-me-more-two', 'fade-in')
+        .setTween(swipe2Tween)
         .addIndicators({
-            name: 'second box slide in',
-            colorTrigger: 'black',
-            colorStart: 'pink'
+           name: 'second box slide in',
+    colorTrigger: 'black',
+        colorStart: 'pink'
         })
         .addTo(controller);
 
-    var ourScene3 = new ScrollMagic.Scene({
-        triggerElement: '.second-box',
-        triggerHook: 0.4
 
+// ----  build tween for  Show Me More 2 box ----- //
+    var swipe3Tween = TweenMax.from("#show-me-more-3", 1.5,
+        {x: -1200, autoAlpha: 0}
+    );
+
+    var swipe3Scene = new ScrollMagic.Scene({
+        triggerElement: '.second-box',
+        triggerHook: 0.3
     })
-        .setClassToggle('#show-me-more-three', 'fade-in')
+        .setTween(swipe3Tween)
         .addIndicators({
             name: 'third slide in',
             colorTrigger: 'black',
             colorStart: 'red'
         })
         .addTo(controller);
+
+
 
     var pinIntroScene = new ScrollMagic.Scene ({
         triggerElement: '.bar',
@@ -92,7 +118,7 @@ $(document).ready(function(){
     var pinBoxes3 = new ScrollMagic.Scene ({
         triggerElement: '.third-box',
         triggerHook: 0,
-        duration: '11%'
+        duration: '16.5%'
     })
         .setPin('.third-box', {pushFollowers: false})
         .addTo(controller);
